c4e003a3c5b914c3c9728da992f93ba85fed4596
x-tropy <buweiliao@gmail.com>
2024-12-17 16:30:12 +0100

    route: add /home#index

---
+++ app/javascript/components/home/Root.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Toolbar from "~/components/ui/Toolbar.vue";
+import HowItWorks from "~/components/HowItWorks.vue"
+
+const items = [
+  {
+    icon: "c-question",
+    label: "How it works",
+    dividerRight: true,
+  },
+  {
+    icon: "n-edit",
+    label: "Editor",
+    dividerRight: true,
+  },
+  {
+    icon: "circle-caret-right",
+    label: "Player",
+  }
+]
+</script>
+
+<template>
+  <div class="h-screen canvas-pattern">
+    <Toolbar :items="items" currentItemIndex="0" />
+    <HowItWorks />
+  </div>
+</template>
+
+<style scoped>
+.canvas-pattern {
+  background-image: url("../../../assets/images/pattern.svg");
+  background-repeat: repeat;
+}
+</style>
--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,7 +1,11 @@
 import {createApp} from "vue";
 import Hello from "../components/Hello.vue"
+import Home from "../components/home/Root.vue"
 
 console.log('Vite ⚡️ Rails')
 
 const app = createApp(Hello)
 app.mount(document.querySelector("#app"))
+
+const appHome = createApp(Home)
+appHome.mount(document.querySelector("#home"))
