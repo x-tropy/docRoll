46f93c0ee04dbc0d3c97ad8d0d9dcd2237926c69
x-tropy <buweiliao@gmail.com>
2024-12-26 14:37:03 +0100

    feature: text to speech (with timestamped transcription).

--- app/controllers/voiceovers_controller.rb
+++ app/controllers/voiceovers_controller.rb
@@ -55,7 +55,7 @@ class VoiceoversController < ApplicationController
     # Create a temporary file with the correct extension
     temp_file = Tempfile.new(["temp_audio", ".mp3"]).tap do |tempfile|
       tempfile.binmode
-      tempfile.write(file.download)  # Download and write the file content
+      tempfile.write(file.download) # Download and write the file content
       tempfile.rewind
     end
 
@@ -88,25 +88,48 @@ class VoiceoversController < ApplicationController
 
   def generate_audio(voiceover)
     # Replace this with actual OpenAI API call logic
-    openai_api_key = ENV["Speech_to_Text"]
-    uri = URI.parse("https://api.openai.com/v1/audio/speech")
+    api_key = ENV["Text_to_Speech"]
+    logger.info "-> -> key: #{api_key}"
+    voice_id = "21m00Tcm4TlvDq8ikWAM" # Replace with the desired voice ID
+    uri = URI.parse("https://api.elevenlabs.io/v1/text-to-speech/#{voice_id}/with-timestamps?output_format=mp3_22050_32")
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/json"
-    request["Authorization"] = "Bearer #{openai_api_key}"
-    request.body = JSON.dump({ "input" => voiceover.text, "voice" => "alloy", "model" => "tts-1" })
+    request["Xi-Api-Key"] = api_key
+    request.body = JSON.dump({
+                               "text" => voiceover.text,
+                               "model_id" => "eleven_turbo_v2",
+                               "voice_settings" => {
+                                 "stability" => 0.5,
+                                 "similarity_boost" => 0.75
+                               }
+                             })
 
     response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
 
     if response.is_a?(Net::HTTPSuccess)
-      audio_data = response.body
+      audio_data = JSON.parse(response.body) # Parse JSON response
+      logger.info "-> -> Response: #{audio_data}"
+
+      # Decode Base64 audio data
+      audio_base64 = audio_data["audio_base64"]
+      decoded_audio = Base64.decode64(audio_base64)
+
+      # Delete the "audio_base64" property
+      # audio_data.delete("audio_base64")
+
+      # Store the remaining response as transcription
+      voiceover.update(transcription: audio_data["alignment"].to_json)
+
+      # Attach decoded audio to Active Storage
       voiceover.file.attach(
-        io: StringIO.new(audio_data),
+        io: StringIO.new(decoded_audio),
         filename: "voiceover_#{voiceover.id}.mp3",
         content_type: "audio/mpeg"
       )
     else
+      logger.error "-> -> Audio generation failed: #{response.body}"
       flash[:alert] = "Audio generation failed: #{response.body}"
     end
   end
