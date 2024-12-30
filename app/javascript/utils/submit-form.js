b122302bfcb6d2714f41f9dce08c4ded8932e67c
x-tropy <buweiliao@gmail.com>
2024-12-30 17:55:36 +0100

    refactor: abstract submit logic as one of the utils function

--- app/javascript/utils/submit-form.js
+++ app/javascript/utils/submit-form.js
@@ -1,14 +1,18 @@
-export default function submitForm(url, formDataObj, csrfTokenMetaName = 'csrf-token') {
-    const formData = new FormData();
+export default function submitForm(url, requestType, formDataObj, csrfTokenMetaName = 'csrf-token') {
+    let formData
 
-    // Populate FormData
-    Object.entries(formDataObj).forEach(([key, value]) => {
-        formData.append(key, value);
-    });
+    if (formDataObj) {
+        formData = new FormData();
+
+        // Populate FormData
+        Object.entries(formDataObj).forEach(([key, value]) => {
+            formData.append(key, value);
+        });
+    }
 
     // Fetch with CSRF token
     return fetch(url, {
-        method: 'POST',
+        method: requestType,
         headers: {
             'X-CSRF-Token': document.querySelector(`meta[name="${csrfTokenMetaName}"]`).content,
             Accept: "application/json",
