1c03beb8f2a9c8c9137576f3d4a033e56744d01a
x-tropy <buweiliao@gmail.com>
2025-01-09 21:05:09 +0100

    fix: handle null

--- app/javascript/utils/prepend-code-language-label.js
+++ app/javascript/utils/prepend-code-language-label.js
@@ -4,7 +4,10 @@ export default function () {
         // avoid duplicate rendering
         if (el.previousElementSibling?.classList.contains('code-language-label')) return
 
-        const language = el.className.match(/language-(\w+)/)[1];
+        const matches = el.className.match(/language-(\w+)/);
+        if (matches === null) return;
+
+        const language = matches[1]
         if (language) {
             const label = document.createElement('span');
             label.textContent = language;
