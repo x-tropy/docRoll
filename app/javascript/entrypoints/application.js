ab55d01c6e287193310f199f9f3e953d8ad0ca58
x-tropy <buweiliao@gmail.com>
2025-01-12 18:34:11 +0100

    feature: working example of subtitles display

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -2,6 +2,7 @@ import {createApp} from "vue";
 import Hello from "../components/Hello.vue"
 import Home from "../components/home/Root.vue"
 import Transcription from "../components/player/Transcription.vue";
+import SubtitlePlayer from "../components/player/SubtitlePlayer.vue";
 
 const app = createApp(Hello)
 app.mount(document.querySelector("#app"))
@@ -15,4 +16,12 @@ if (transcriptionDiv) {
         transcription: transcriptionDiv.getAttribute("transcription")
     })
     appTranscription.mount(transcriptionDiv)
+}
+
+const SubtitlePlayerDiv = document.querySelector('#SubtitlePlayer')
+if (SubtitlePlayerDiv) {
+    createApp(SubtitlePlayer, {
+        subtitles: SubtitlePlayerDiv.dataset.subtitles,
+        audioUrl: SubtitlePlayerDiv.dataset.audioUrl
+    }).mount(SubtitlePlayerDiv)
 }
